a6f890b9b4c7ddf4c8845368ce533214b71e077e
jon4hz <me@jon4hz.io>
2023-03-04 18:59:33 +0100

    add 3 node to cluster

--- environments/prod/kubernetes.tf
+++ environments/prod/kubernetes.tf
@@ -13,8 +13,8 @@ locals {
         {
           name           = "np-d2-4-f1"
           flavor_name    = "d2-4"
-          max_nodes      = 2
-          min_nodes      = 2
+          max_nodes      = 3
+          min_nodes      = 3
           monthly_billed = true
           autoscale      = false
           labels = {
